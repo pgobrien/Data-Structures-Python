941b2b1ba8742c707236c1527b20fb911d5ce22a
pgobrien <pgobrien@iu.edu>
2019-06-20 21:35:39 -0400

    Hmmm

--- CTCIChapter1.py
+++ CTCIChapter1.py
@@ -2,7 +2,35 @@
 
 
 def is_unique(a_string):
-    pass
+    # with a data structure
+    counts = {}
+    for c in a_string:
+        if c in counts:
+            return False
+        else:
+            counts[c] = 1
+    return True
+
+def is_unique_v2(a_string):
+    # without a data structure
+    sorted_string = ''.join(sorted(a_string))
+    print(sorted_string[0])
+    print(sorted_string[1])
+    if sorted_string[0] == sorted_string[1]:
+        return False
+    else:
+        return True
+
+print(is_unique("Hey"))
+print(is_unique("Hello"))
+print(is_unique("Help"))
+print(is_unique("Naah"))
+print()
+# Not passing due to casing
+print(is_unique_v2("Hey"))
+print(is_unique_v2("Hello"))
+print(is_unique_v2("Help"))
+print(is_unique_v2("Naah"))
 
 
 def check_permutation(string_one, string_two):
