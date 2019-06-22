50b9871e602abe3beee8e571023da13fa7b840a3
pgobrien <pgobrien@iu.edu>
2019-06-22 18:52:20 -0400

    Fixed Bug in Queue

--- HashTable.py
+++ HashTable.py
@@ -1,10 +1,33 @@
 from Exceptions import *
+import AbstractHash
 
-class HashTable:
+class HashTable(AbstractHash):
+    """Hash Map implemented using Separate Chaining"""
 
-    def __init__(self, size):
-        self.internalList = []
+    def _bucket_getitem(self, j, k):
+        bucket = self._table[j]
+        if bucket is None:
+            raise KeyError('Key Error: ' + repr(k))
+        return bucket[k]
 
-        for i in range(size):
-            pass
 
+    def _bucket_setitem(self, j, k, v):
+        if self._table[j] is None:
+            self._table[j] = UnsortedTableMap()
+        oldsize = len(self._table[j])
+        self._table[j][k] = v
+        if len(self._table[j]) > oldsize:
+            self._n += 1
+
+
+    def _bucket_delitem(self, j, k):
+        bucket = self._table[j]
+        if bucket is None:
+            raise KeyError('Key Error: ' + repr(k))
+        del bucket[k]
+
+    def __iter__(self):
+        for bucket in self._table:
+            if bucket is not None:
+                for key in bucket:
+                    yield key
