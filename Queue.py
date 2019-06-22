7e2cdfafc564a8b5c985f05f0e012b631a9d87b4
pgobrien <pgobrien@iu.edu>
2019-06-21 21:06:11 -0400

    Fixed Bug in Queue

--- Queue.py
+++ Queue.py
@@ -16,7 +16,7 @@ class MyQueue(object):
     def add(self, item):
         new_node = QueueNode(item)
         if self.last is not None:
-            self.last = new_node
+            self.last.next = new_node
         self.last = new_node
         if self.first is None:
             self.first = self.last
@@ -25,11 +25,12 @@ class MyQueue(object):
     def remove(self):
         if self.first is None:
             raise EmptyStackException
-        data = self.first.data
+        node_data = self.first.data
         self.first = self.first.next
         if self.first is None:
             self.last = None
-        return data
+        return node_data
+
 
     def peek(self):
         if self.first is None:
@@ -39,14 +40,12 @@ class MyQueue(object):
     def is_empty(self):
         return self.first is None
 
-
-
 test_queue = MyQueue()
 
 for i in range(10):
     test_queue.add(i)
 
-for i in range(8):
+for i in range(10):
     data = test_queue.remove()
     print(data)
 
